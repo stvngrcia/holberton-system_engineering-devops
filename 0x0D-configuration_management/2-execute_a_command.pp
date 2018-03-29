6952df073f51f75b3bf5533664632323838e3838
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-29 16:30:24 -0700

    removing flag

--- 0x0D-configuration_management/2-execute_a_command.pp
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Kill a process by name
 exec { 'killmenow':
     path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-    command => 'pkill -15 -f killmenow',
+    command => 'pkill -f killmenow',
 }
